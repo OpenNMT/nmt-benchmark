559ee072d2f280a41a9a60e497bf8c1177352bcc
AKHANOV Egor <egor.akhanov@systrangroup.com>
2017-01-20 16:32:40 +0100

    Add domain column to test file list

--- public/javascripts/testFiles.js
+++ public/javascripts/testFiles.js
@@ -25,13 +25,13 @@ function getTable (languagePair) {
     data: filterByLp(testSets, languagePair),
     order: [0, 'asc'],
     columns: [
-      {data: 'languagePair', render: function (data, type, full) {
-        // LP to human readable format
-        return full.source.language + full.target.language
+      {data: 'languagePair', sWidth: '130', render: function (data, type, full) {
+        return [c2l[full.source.language], c2l[full.target.language]].join(' - ');
       }},
-      {data: 'source.fileName'},
+      {data: 'domain'},
+      {data: 'source.fileName', sWidth: '130'},
       {data: 'download', sortable: false, sDefaultContent: '', render: function (data, type, full) {
-        return '<div class="downloadSrc circular ui icon button" data-fileId="' + full._id + '"><i class="download icon"></i></div>'
+        return '<div class="downloadSrc circular ui basic icon button" data-fileId="' + full._id + '"><i class="download icon"></i></div>'
       }}
     ],
     drawCallback: function (settings) {
@@ -54,8 +54,8 @@ function filterByLp (list, languagePair) {
   if (languagePair) {
     var src = languagePair.substring(0,2);
     var tgt = languagePair.substring(2);
-    return list.filter(function (itme) {
-      return itme.sourceLanguage == src && itme.targetLanguage == tgt;
+    return list.filter(function (item) {
+      return item.source.language === src && item.target.language === tgt;
     });
   } else {
     return list;
