2178fc9ea3707cd88dc1171d3f1edd50d15d80d0
AKHANOV Egor <egor.akhanov@systrangroup.com>
2017-01-24 13:08:53 +0100

    Refs #45193 - Add more options to system submit form

--- config/systemSubmitForm.js
+++ config/systemSubmitForm.js
@@ -61,6 +61,142 @@ var systemDescription = [
     "label": "Recipe",
     "required": false,
     "layer": 1
+  },
+
+  {
+    "name": "architecture",
+    "default": "seq2seq-attn",
+    "placeholder": "",
+    "type": "text",
+    "label": "Global NN architecture",
+    "required": false,
+    "layer": 2
+  },
+  {
+    "name": "features",
+    "default": "case",
+    "placeholder": "",
+    "type": "text",
+    "label": "Use of side features",
+    "required": false,
+    "layer": 2
+  },
+  {
+    "name": "tokenization",
+    "default": "generic+BPE",
+    "placeholder": "",
+    "type": "text",
+    "label": "Tokenization type",
+    "required": false,
+    "layer": 2
+  },
+  {
+    "name": "vocabulary",
+    "default": "50000/50000",
+    "placeholder": "",
+    "type": "text",
+    "label": "Vocabulary Size",
+    "required": false,
+    "layer": 2
+  },
+  {
+    "name": "layers",
+    "default": "2x4",
+    "placeholder": "",
+    "type": "text",
+    "label": "Number of layers",
+    "required": false,
+    "layer": 2
+  },
+  {
+    "name": "rnnType",
+    "default": "1000 LSTM",
+    "placeholder": "",
+    "type": "text",
+    "label": "RNN type",
+    "required": false,
+    "layer": 2
+  },
+  {
+    "name": "dropout",
+    "default": "0.3",
+    "placeholder": "",
+    "type": "text",
+    "label": "Dropout",
+    "required": false,
+    "layer": 2
+  },
+  {
+    "name": "embedding",
+    "default": "600",
+    "placeholder": "",
+    "type": "text",
+    "label": "Word Embedding",
+    "required": false,
+    "layer": 2
+  },
+  {
+    "name": "encoder",
+    "default": "",
+    "placeholder": "",
+    "type": "text",
+    "label": "Encoder specific",
+    "required": false,
+    "layer": 3
+  },
+  {
+    "name": "decoder",
+    "default": "",
+    "placeholder": "",
+    "type": "text",
+    "label": "Decoder specific",
+    "required": false,
+    "layer": 3
+  },
+  {
+    "name": "attention",
+    "default": "Global Attention",
+    "placeholder": "",
+    "type": "text",
+    "label": "Attention specific",
+    "required": false,
+    "layer": 3
+  },
+  {
+    "name": "generator",
+    "default": "SoftMax",
+    "placeholder": "",
+    "type": "text",
+    "label": "Generator",
+    "required": false,
+    "layer": 3
+  },
+  {
+    "name": "oov",
+    "default": "",
+    "placeholder": "",
+    "type": "text",
+    "label": "OOV Replacement",
+    "required": false,
+    "layer": 3
+  },
+  {
+    "name": "optimization",
+    "default": "sgd",
+    "placeholder": "",
+    "type": "text",
+    "label": "Optimization",
+    "required": false,
+    "layer": 3
+  },
+  {
+    "name": "training",
+    "default": "13 epochs",
+    "placeholder": "",
+    "type": "text",
+    "label": "Training Specific",
+    "required": false,
+    "layer": 3
   }
 ];
 
