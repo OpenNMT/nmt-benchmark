5a748e53b76e1cc509916aa6deeeaa18db6a8e00
AKHANOV Egor <egor.akhanov@systrangroup.com>
2017-01-27 17:08:37 +0100

    Revisit training set page

--- public/javascripts/trainingSets.js
+++ public/javascripts/trainingSets.js
@@ -8,30 +8,18 @@ $(document).ready(function () {
     target: '#trainingSetTable',
     type: 'training',
     columns: [
-      {data: 'languagePair', sWidth: '130', render: function (data, type, full) {
-        return [c2l[full.source.language], c2l[full.target.language]].join(' - ');
+      {data: 'languagePair', render: function (data, type, full) {
+        return [c2l[full.source.language], c2l[full.target.language]].join('&nbsp;-&nbsp;');
       }},
-      {data: 'fileName', sWidth: '130', render: function (data, type, full) {
+      {data: 'fileName', render: function (data, type, full) {
         return full.fileName;
       }},
-      /*{data: 'size', render: function (data, type, full) {
-        return '-';
-      }},*/
       {data: 'download', sortable: false, sDefaultContent: '', render: function (data, type, full) {
         return '<div class="downloadSrc circular ui basic icon fireBrick button" data-fileName="' + full.fileName + '.tgz"><i class="download icon"></i></div>';
       }}
     ]
   }
 
-  // Language pair selection handler
-  $('#languagePairs').dropdown({
-    onChange: function (value, text) {
-      if (!value) {
-        $('#languagePairs').dropdown('restore defaults');
-      }
-      getTable(filterByLp(trainingSets, value), dtConfig);
-    }
-  });
-
+  // Draw table
   getTable(filterByLp(trainingSets, null), dtConfig);
 });
