3517a7a1242a547dadb0fa865fad8745552df08e
AKHANOV Egor <egor.akhanov@systrangroup.com>
2017-01-27 17:23:12 +0100

    Refs #45193 - Revisit 'Test Sets' page

--- public/javascripts/testSets.js
+++ public/javascripts/testSets.js
@@ -8,12 +8,12 @@ $(document).ready(function () {
     target: '#testSetTable',
     type: 'test',
     columns: [
-      {data: 'languagePair', sWidth: '130', render: function (data, type, full) {
-        return [c2l[full.source.language], c2l[full.target.language]].join(' - ');
+      {data: 'languagePair', render: function (data, type, full) {
+        return [c2l[full.source.language], c2l[full.target.language]].join('&nbsp;-&nbsp;');
       }},
       {data: 'domain'},
       {data: 'origin'},
-      {data: 'source.fileName', sWidth: '130'},
+      {data: 'source.fileName'},
       {data: 'download', sortable: false, sDefaultContent: '', render: function (data, type, full) {
         return '<div class="downloadSrc circular ui basic icon fireBrick button" data-fileId="' + full._id + '"><i class="download icon"></i></div>'
       }}
@@ -30,5 +30,6 @@ $(document).ready(function () {
     }
   });
 
+  // Draw table
   getTable(filterByLp(testSets, null), dtConfig);
 });
