8da215d07b94263f6a84030d016101f8a1ebab25
AKHANOV Egor <egor.akhanov@systrangroup.com>
2017-02-03 18:49:51 +0100

    Refs #45193 - lint templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const jshint = require('gulp-jshint');
+const jshintStylish = require('jshint-stylish');
 const jscs = require('gulp-jscs');
 const jscsStylish = require('gulp-jscs-stylish');
-const jshintStylish = require('jshint-stylish');
-const cached = require('gulp-cached');
-const remember = require('gulp-remember');
-const rename = require("gulp-rename");
+const puglint = require('gulp-pug-lint');
 const spawn = require('child_process').spawn;
 const log = require('gulp-util').log;
+const cached = require('gulp-cached');
+const remember = require('gulp-remember');
+const rename = require('gulp-rename');
 
 var server;
 const path = {
@@ -20,21 +21,22 @@ const path = {
     config: './source/.jshintrc',
     js: 'source/javascripts/*.js',
     css: 'source/stylesheets',
-    pug: 'view/*.pug'
+    templates: 'views/*.pug'
   }
 };
 
 /**
- * Build browser resources: javascript files, stylesheets, jade templates
+ * Build browser resources: javascript files, stylesheets
  */
-gulp.task('browser:lint', function () {
+gulp.task('browser:jslint', function () {
   return gulp.src([path.browser.config, path.browser.js])
     .pipe(jshint())
     .pipe(jshint.reporter(jshintStylish))
     .pipe(jshint.reporter('fail', {ignoreWarning: false}));
 });
-gulp.task('browser:jscs', ['browser:lint'], function () {
+gulp.task('browser:jscs', ['browser:jslint'], function () {
   return gulp.src(path.browser.js)
+    // only changed files
     .pipe(jscs())
     .pipe(jscsStylish())
     .pipe(jscs.reporter('failImmediately'))
@@ -47,12 +49,20 @@ gulp.task('browser:minify', ['browser:jscs'], function () {
     .pipe(gulp.dest('./public/javascripts'))
     .pipe(remember('scripts'));
 });
-gulp.task('build:browser', ['browser:minify'], function () {
-  log('Javascript files linted and minified');
+gulp.task('build:browser', ['browser:minify'], function () {});
+
+/**
+ * Lint pug templates
+ */
+gulp.task('browser:puglint', function () {
+  gulp.src(path.browser.templates)
+    .pipe(cached('templates'))
+    .pipe(puglint())
+    .pipe(remember('templates'));
 });
 
 /**
- * Lint server code
+ * Related to server
  */
 gulp.task('server:lint', function () {
   return gulp.src([path.server.config, ...path.server.app])
@@ -60,38 +70,14 @@ gulp.task('server:lint', function () {
     .pipe(jshint.reporter(jshintStylish))
     .pipe(jshint.reporter('fail', {ignoreWarning: false}));
 });
-gulp.task('server:jscs', ['server:lint'], function () {
+gulp.task('server:jscs', function () {
   return gulp.src(path.server.app)
+    // only changed files
     .pipe(jscs())
     .pipe(jscsStylish())
     .pipe(jscs.reporter('failImmediately'));
 });
-gulp.task('build:server', ['server:jscs'], function () {
-  log('Server linted');
-});
-
-gulp.task('watch', ['server'], function () {
-  var watch = {
-    browser: {},
-    server: {}
-  };
-  watch.browser.js = gulp.watch(path.browser.js, ['build:browser']); // css + jade
-  // var watch.browser.css = gulp.watch(path.browser.css, ['browser']);
-  // var watch.browser.pug = gulp.watch(path.browser.pug, ['browser']);
-
-  // Server livereload
-  watch.server = gulp.watch(path.server.app, ['server']);
-
-  // Browser livereload
-  watch.browser.js.on('change', function (e) {
-    if (e.type === 'deleted') {
-      delete cached.caches.scripts[e.path];
-      remember.forget('scripts', e.path);
-    }
-  });
-});
-
-gulp.task('server', ['build:server'], function () {
+gulp.task('server:start', ['server:lint', 'server:jscs'], function () {
   log('Reload server...');
   killServer();
   server = spawn('node', ['./bin/www'], {stdio: 'inherit'});
@@ -102,12 +88,47 @@ gulp.task('server', ['build:server'], function () {
   });
 });
 
+/**
+ * Entry point
+ */
+gulp.task('default', ['server:start'], function () {
+  var watch = {
+    browser: {},
+    server: {}
+  };
+  watch.browser.js = gulp.watch(path.browser.js, ['build:browser']);
+  // watch.browser.css = gulp.watch(path.browser.css, ['browser']); - TODO
+
+  // Lint pug templates watcher
+  watch.browser.templates = gulp.watch(path.browser.templates, ['browser:puglint']);
+  watch.browser.templates.on('change', function (e) {
+    if (e.type === 'deleted') {
+      delete cached.caches.templates[e.path];
+      remember.forget('templates', e.path);
+    }
+  });
+
+  // Server livereload
+  watch.server = gulp.watch(path.server.app, ['server:start']);
+
+  // Browser livereload - TODO
+  watch.browser.js.on('change', function (e) {
+    if (e.type === 'deleted') {
+      delete cached.caches.scripts[e.path];
+      remember.forget('scripts', e.path);
+    }
+  });
+});
+
+
 process.on('SIGINT', function () {
   killServer();
+  process.exit();
 });
 
 process.on('SIGTERM', function () {
   killServer();
+  process.exit();
 });
 
 function killServer () {
