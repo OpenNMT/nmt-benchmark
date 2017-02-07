951378bd8d83a9be25831e55802de8b1f246ba5d
AKHANOV Egor <egor.akhanov@systrangroup.com>
2017-02-07 14:42:34 +0100

    Set all globals at once

--- routes/globals/getDefaultLP.js
+++
@@ -1,9 +0,0 @@
-var router = require('express').Router();
-var nconf = require('nconf');
-
-router.use('/', function (req, res, next) {
-  res.locals.defaultLP = nconf.get('OpenNMTBenchmark:default:LP');
-  next();
-});
-
-module.exports = router;
--- routes/globals/getUser.js
+++
@@ -1,9 +0,0 @@
-var router = require('express').Router();
-var nconf = require('nconf');
-
-router.use('/', function (req, res, next) {
-  res.locals.visitor = req.user || '';
-  next();
-});
-
-module.exports = router;
