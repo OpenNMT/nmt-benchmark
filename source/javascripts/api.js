a52654a521efd166cd9a250051ad0707c31466af
AKHANOV Egor <egor.akhanov@systrangroup.com>
2017-02-07 16:53:54 +0100

    Copy apiKey on click

---
+++ source/javascripts/api.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $('#copyApiKey').on('click', function () {
+    var range;
+    if (document.selection) {
+      range = document.body.createTextRange();
+      range.moveToElementText(document.getElementById('apiKey'));
+      range.select().createTextRange();
+      document.execCommand('Copy');
+    } else if (window.getSelection) {
+      range = document.createRange();
+      range.selectNode(document.getElementById('apiKey'));
+      window.getSelection().addRange(range);
+      document.execCommand('Copy');
+    }
+  });
+});
