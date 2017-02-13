dbe3d993a9e731d589e0e792708c17a6cb269bcb
AKHANOV Egor <egor.akhanov@systrangroup.com>
2017-02-13 13:09:03 +0100

    Refs #45193 - Add system id to description

--- source/javascripts/api.js
+++ source/javascripts/api.js
@@ -1,18 +1,4 @@
 $(document).ready(function () {
   $('#copyApiKey').popup();
-  $('#copyApiKey').on('click', function () {
-    var range;
-    if (document.selection) {
-      range = document.body.createTextRange();
-      range.moveToElementText(document.getElementById('apiKey'));
-      range.select().createTextRange();
-      document.execCommand('Copy');
-    } else if (window.getSelection) {
-      window.getSelection().removeAllRanges();
-      range = document.createRange();
-      range.selectNode(document.getElementById('apiKey'));
-      window.getSelection().addRange(range);
-      document.execCommand('Copy');
-    }
-  });
+  click2copy('copyApiKey', 'apiKey');
 });
