f392c20d46a46856173f0742a443ccb829c7b992
AKHANOV Egor <egor.akhanov@systrangroup.com>
2017-02-15 18:33:19 +0100

    Refs #45193 - Add constrained system mark in user systems

--- source/javascripts/userSystems.js
+++ source/javascripts/userSystems.js
@@ -14,7 +14,19 @@ $(document).ready(function () {
 function getTable () {
   var columns = [
     {data: 'systemName', className: 'systemName', sDefaultContent: '', render: function (data, type, full) {
-      return ('<a href="/translationSystem/view/' + full._id + '">' + data + '</a>');
+      var c = {
+        'true': {
+          content: 'Trained on internal data',
+          toggle: 'on'
+        },
+        'false': {
+          content: 'Trained on custom data',
+          toggle: 'off'
+        }
+      };
+      if (data) {
+        return ('<i class="toggle ' + c[full.constraint].toggle + ' icon" data-content="' + c[full.constraint].content + '" data-position="top center"></i><a href="/translationSystem/view/' + full._id + '">' + data + '</a>');
+      }
     }},
     {data: 'date', sDefaultContent: '', render: function (data, type, full) {
       if (data) {
@@ -55,6 +67,7 @@ function getTable () {
     columns: columns,
     drawCallback: function (settings) {
       $('.pagination.menu').addClass('floated right');
+      $('i.toggle').popup();
     }
   });
 }
